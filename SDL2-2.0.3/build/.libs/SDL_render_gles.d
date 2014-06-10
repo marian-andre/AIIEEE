caa30c43319ecd849f06ff2fd7c797f28056b323
mlemort <lemort.maxime@icloud.com>
2014-06-10 23:05:15 +0200

    All

--- SDL2-2.0.3/build/.libs/SDL_render_gles.d
+++
@@ -1,7 +0,0 @@
-build/SDL_render_gles.lo: \
-  /nfs/zfs-student-4/users/2013/rkorimba/AIIEEE/SDL2-2.0.3/src/render/opengles/SDL_render_gles.c \
-  /nfs/zfs-student-4/users/2013/rkorimba/AIIEEE/SDL2-2.0.3/src/render/opengles/../../SDL_internal.h \
-  /nfs/zfs-student-4/users/2013/rkorimba/AIIEEE/SDL2-2.0.3/src/render/opengles/../../dynapi/SDL_dynapi.h \
-  /nfs/zfs-student-4/users/2013/rkorimba/AIIEEE/SDL2-2.0.3/src/render/opengles/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h
--- SDL2-2.0.3/build/SDL_string.d
+++
@@ -1,7 +0,0 @@
-build/SDL_string.lo: \
-  /nfs/zfs-student-4/users/2013/rkorimba/AIIEEE/SDL2-2.0.3/src/stdlib/SDL_string.c \
-  /nfs/zfs-student-4/users/2013/rkorimba/AIIEEE/SDL2-2.0.3/src/stdlib/../SDL_internal.h \
-  /nfs/zfs-student-4/users/2013/rkorimba/AIIEEE/SDL2-2.0.3/src/stdlib/../dynapi/SDL_dynapi.h \
-  /nfs/zfs-student-4/users/2013/rkorimba/AIIEEE/SDL2-2.0.3/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_stdinc.h

d0becfe08acd49583e1a2217e16c7a9a6d22ef1f
rkorimba <korimbacus.riad@gmail.com>
2014-06-10 16:59:56 +0200

    up compil

--- srcs/serveur/ft_take.c
+++ srcs/serveur/ft_take.c
@@ -6,7 +6,7 @@
 /*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/04 16:34:22 by jgranet           #+#    #+#             */
-/*   Updated: 2014/06/10 13:48:08 by jgranet          ###   ########.fr       */
+/*   Updated: 2014/06/10 16:59:36 by rkorimba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,20 @@ void		ft_take(t_cmd *cmd, t_game *g)
 
 	if (g->cls[cmd->num_cli].cs == cmd->fd)
 	{
-		split = ft_strsplit(cmd, ' ');
+		split = ft_strsplit(cmd->cmd, ' ');
 		if (ft_strcmp("linemate", split[1]) == 0)
-		{
-			;
-		}
+			return ;
 		else if (ft_strcmp("deraumere", split[1]) == 0)
-			;
+			return ;
 		else if (ft_strcmp("sibur", split[1]) == 0)
-			;
+			return ;
 		else if (ft_strcmp("mendiane", split[1]) == 0)
-			;
+			return ;
 		else if (ft_strcmp("phiras", split[1]) == 0)
-			;
+			return ;
 		else if (ft_strcmp("thystame", split[1]) == 0)
-			;
+			return ;
 		else if (ft_strcmp("food", split[1]) == 0)
-			;
+			return ;
 	}
 }

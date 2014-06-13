8ac9fd9f157c3e34560885a451d297d611983602
Jean-Daniel GRANET <jgranet@e1r8p9.42.fr>
2014-06-13 18:32:10 +0200

    pbc

--- srcs/serveur/ft_graph_pbc.c
+++ srcs/serveur/ft_graph_pbc.c
@@ -6,17 +6,27 @@
 /*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/06 14:01:34 by jgranet           #+#    #+#             */
-/*   Updated: 2014/06/06 14:03:36 by jgranet          ###   ########.fr       */
+/*   Updated: 2014/06/13 18:30:47 by jgranet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "serveur.h"
 #include "libft.h"
 
-void		ft_graph_pbc(t_game *g, int i, int num_cli, char *msg)
+void		ft_graph_pbc(t_game *g, int num_cli, char *msg)
 {
-	ft_putstr_fd("pbc ", g->cls[i].cs);
-	ft_putnbr_fd(num_cli, g->cls[i].cs);
-	ft_putchar_fd(' ', g->cls[i].cs);
-	ft_putendl_fd(msg, g->cls[i].cs);
+	int		i;
+
+	i = 0;
+	while (g->cls[i].cs)
+	{
+		if (g->cls[i].graph == 1)
+		{
+			ft_putstr_fd("pbc ", g->cls[i].cs);
+			ft_putnbr_fd(num_cli, g->cls[i].cs);
+			ft_putchar_fd(' ', g->cls[i].cs);
+			ft_putendl_fd(msg, g->cls[i].cs);
+		}
+		i++;
+	}
 }

4a20cdccfc917b93cef83b7405cbc7af46cf0b73
mlemort <lemort.maxime@icloud.com>
2014-06-13 20:24:18 +0200

    broadcast

--- srcs/serveur/ft_broadcast.c
+++ srcs/serveur/ft_broadcast.c
@@ -6,7 +6,7 @@
 /*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/04 16:34:22 by jgranet           #+#    #+#             */
-/*   Updated: 2014/06/13 19:37:15 by mlemort          ###   ########.fr       */
+/*   Updated: 2014/06/13 20:23:35 by mlemort          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,44 @@
 #include "serveur.h"
 #include "libft.h"
 
+static int		ft_dir(t_game *g, int num_cli, int card)
+{
+	int		dir;
+
+	dir = g->cls[num_cli].dir;
+	if ((dir == NORTH && card == SOUTH) || (dir == SOUTH && card == NORTH)
+		|| (dir == WEST && card == EAST) || (dir == EAST && card == WEST))
+		return (5);
+	if ((dir == EAST && card == NORTH) || (dir == SOUTH && card == EAST)
+		|| (dir == WEST && card == SOUTH) || (dir == NORTH && card == WEST))
+		return (3);
+	if ((dir == NORTH && card == NORTH) || (dir == SOUTH && card == SOUTH)
+		|| (dir == WEST && card == WEST) || (dir == EAST && card == EAST))
+		return (1);
+	return (7);
+}
+
 static int		ft_get_k(t_game *g, int num_cli, int i)
 {
-	(void)g;
-	(void)num_cli;
-	(void)i;
-	return (0);
+	t_var	var;
+
+	var.new_x = g->cls[num_cli].x - g->cls[i].x;
+	var.new_y = g->cls[num_cli].y - g->cls[i].y;
+	if (abs(var.new_x) < abs(var.new_y))
+	{
+		if (var.new_y > 0)
+			return (ft_dir(g, i, SOUTH));
+		else
+			return (ft_dir(g, i, NORTH));
+	}
+	}
+	else
+	{
+		if (var.new_x > 0)
+			return (ft_dir(g, i, EAST));
+		else
+			return (ft_dir(g, i, WEST));
+	}
 }
 
 void			ft_broadcast(t_cmd *cmd, t_game *g)

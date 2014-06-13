1e85570ca47d11f6389ae66db8aaa467f5f6726c
Yann OREAL <y.oreal@yahoo.fr>
2014-06-13 15:56:17 +0200

    send maj food au cli graph

--- srcs/serveur/ft_up_food.c
+++ srcs/serveur/ft_up_food.c
@@ -6,12 +6,26 @@
 /*   By: yoreal <yoreal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/05 18:25:02 by yoreal            #+#    #+#             */
-/*   Updated: 2014/06/11 12:10:28 by yoreal           ###   ########.fr       */
+/*   Updated: 2014/06/13 15:51:50 by yoreal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <time.h>
 #include "serveur.h"
+
+static void	ft_loop_map(t_game *game, int x, int y)
+{
+	y = -1;
+	while (++y < game->height)
+	{
+		x = -1;
+		while (++x < game->width)
+		{
+			if (game->map[y][x].food == 0)
+				game->map[y][x].food = ft_rand(Q_FO);
+		}
+	}
+}
 
 void		ft_up_food(t_game *game)
 {
@@ -29,15 +43,7 @@ void		ft_up_food(t_game *game)
 	}
 	if (food < (game->height * game->width) * (Q_FO / 2))
 	{
-		y = -1;
-		while (++y < game->height)
-		{
-			x = -1;
-			while (++x < game->width)
-			{
-				if (game->map[y][x].food == 0)
-					game->map[y][x].food = ft_rand(Q_FO);
-			}
-		}
+		ft_loop_map(game, x, y);
+		ft_graph_mct(game, 0);
 	}
 }

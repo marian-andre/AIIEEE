61ac4f1d72943167854b1f60d5ed68c7d84c4e9c
Yann OREAL <y.oreal@yahoo.fr>
2014-06-14 16:32:25 +0200

    check MORT dans ft_spell

--- srcs/serveur/ft_spell.c
+++ srcs/serveur/ft_spell.c
@@ -6,7 +6,7 @@
 /*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/04 16:34:22 by jgranet           #+#    #+#             */
-/*   Updated: 2014/06/13 16:20:21 by yoreal           ###   ########.fr       */
+/*   Updated: 2014/06/14 16:30:05 by yoreal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ static int	ft_co_wizards(t_cmd *cmd, t_game *g, int lvl_up)
 	{
 		if (g->cls[i].x == g->cls[cmd->num_cli].x
 			&& g->cls[i].y == g->cls[cmd->num_cli].y
-			&& g->cls[i].lvl == g->cls[cmd->num_cli].lvl)
+			&& g->cls[i].lvl == g->cls[cmd->num_cli].lvl
+			&& g->cls[i].cs != MORT)
 		{
 			nb++;
 			if (lvl_up == 1)
@@ -66,6 +67,8 @@ void		ft_spell(t_cmd *cmd, t_game *g)
 {
 	int		nb;
 
+	if (g->cls[cmd_num_cli].cs == MORT)
+		return ;
 	nb = ft_co_wizards(cmd, g, 0);
 	ft_graph_pic(g, cmd->num_cli);
 	if (g->cls[cmd->num_cli].lvl == 1)

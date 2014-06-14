e3b272f65ba11be3db25bf3bac4eedb2c079a1a9
rkorimba <korimbacus.riad@gmail.com>
2014-06-14 15:38:46 +0200

    up pgt et pdr

--- srcs/gfx/ft_graph_pdr.c
+++ srcs/gfx/ft_graph_pdr.c
@@ -6,17 +6,41 @@
 /*   By: rkorimba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/12 16:03:05 by rkorimba          #+#    #+#             */
-/*   Updated: 2014/06/14 15:01:29 by rkorimba         ###   ########.fr       */
+/*   Updated: 2014/06/14 15:37:35 by rkorimba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "graphic.h"
 
-void		ft_graph_pdr(t_game *game, char *line)
+static void		ft_core_pdr(t_game *game, int i, char **tab)
 {
-	int		i;
-	char	**tab;
+	if (ft_atoi(tab[2]) == 3 && game->client[i].resource.sibur > 0)
+	{
+		game->map[game->client[i].y][game->client[i].x].sibur++;
+		game->client[i].resource.sibur--;
+	}
+	if (ft_atoi(tab[2]) == 4 && game->client[i].resource.mendiane > 0)
+	{
+		game->map[game->client[i].y][game->client[i].x].mendiane++;
+		game->client[i].resource.mendiane--;
+	}
+	if (ft_atoi(tab[2]) == 5 && game->client[i].resource.phiras > 0)
+	{
+		game->map[game->client[i].y][game->client[i].x].phiras++;
+		game->client[i].resource.phiras--;
+	}
+	if (ft_atoi(tab[2]) == 6 && game->client[i].resource.thystame > 0)
+	{
+		game->map[game->client[i].y][game->client[i].x].thystame++;
+		game->client[i].resource.thystame--;
+	}
+}
+
+void			ft_graph_pdr(t_game *game, char *line)
+{
+	int			i;
+	char		**tab;
 
 	i = ft_atoi(tab[1]);
 	if ((tab = ft_strsplit(line, ' ')) == NULL)
@@ -24,17 +48,16 @@ void		ft_graph_pdr(t_game *game, char *line)
 	if (game->client[i].lvl > 0)
 	{
 		if (ft_atoi(tab[2]) == 1 && game->client[i].resource.linemate > 0)
+		{
+			game->map[game->client[i].y][game->client[i].x].linemate++;
 			game->client[i].resource.linemate--;
-		else if (ft_atoi(tab[2]) == 2 && game->client[i].resource.deraumere > 0)
+		}
+		if (ft_atoi(tab[2]) == 2 && game->client[i].resource.deraumere > 0)
+		{
+			game->map[game->client[i].y][game->client[i].x].deraumere++;
 			game->client[i].resource.deraumere--;
-		else if (ft_atoi(tab[2]) == 3 && game->client[i].resource.sibur > 0)
-			game->client[i].resource.sibur--;
-		else if (ft_atoi(tab[2]) == 4 && game->client[i].resource.mendiane > 0)
-			game->client[i].resource.mendiane--;
-		else if (ft_atoi(tab[2]) == 5 && game->client[i].resource.phiras > 0)
-			game->client[i].resource.phiras--;
-		else if (ft_atoi(tab[2]) == 6 && game->client[i].resource.thystame > 0)
-			game->client[i].resource.thystame--;
+		}
+		ft_core_pdr(game, i, tab);
 	}
 	ft_strdel2(&tab);
 }

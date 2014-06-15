0ae987a7816e202e5c048288d6c6f1d7ceda3444
Jean-Daniel GRANET <jgranet@e1r8p9.42.fr>
2014-06-15 19:31:41 +0200

    modif ft_life

--- srcs/serveur/ft_life.c
+++ srcs/serveur/ft_life.c
@@ -6,7 +6,7 @@
 /*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/11 15:12:45 by jgranet           #+#    #+#             */
-/*   Updated: 2014/06/13 18:08:57 by mlemort          ###   ########.fr       */
+/*   Updated: 2014/06/15 19:31:19 by jgranet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,11 @@ void		ft_life(t_cmd *cmd, t_game *g)
 		ft_putnbr(g->cls[cmd->num_cli].resource.food);
 		ft_putstr(", t = ");
 		ft_putnbr(g->tab[11].t);
+		ft_putstr(", client num ");
+		ft_putnbr(cmd->num_cli);
 		ft_putchar('\n');
 		g->cls[cmd->num_cli].resource.food--;
-		if (g->cls[cmd->num_cli].resource.food == 0)
+		if (g->cls[cmd->num_cli].resource.food <= 0)
 			ft_death(cmd, g);
 		else
 		{

12b6873dc0800f6bc0467f839e711d04ff9a598d
rkorimba <korimbacus.riad@gmail.com>
2014-06-17 19:42:23 +0200

    ft_strdup

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/27 21:03:03 by jgranet           #+#    #+#             */
-/*   Updated: 2014/04/27 21:03:03 by jgranet          ###   ########.fr       */
+/*   Updated: 2014/06/17 19:40:39 by rkorimba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 
 char	*ft_strdup(const char *s1)
 {
-	char	*s2;
+	int		i;
+	char	*dest;
 
+	dest = NULL;
 	if (s1)
 	{
-		s2 = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
-		if (s2)
-		{
-			ft_strcpy(s2, s1);
-			return (s2);
-		}
+		if ((dest = (char*)malloc(sizeof(char) * (ft_strlen(s1) + 1))) == NULL)
+			return (NULL);
+		i = -1;
+		while (s1[++i])
+			dest[i] = s1[i];
+		dest[i] = '\0';
 	}
-	return (NULL);
+	return (dest);
 }

84be6d8ba8a52336e2b2ae6801ae0155d1ec9630
Cyprien CHAUVIERE <cchauvie@e1r8p7.42.fr>
2014-06-21 16:50:41 +0200

    update verbose client

--- srcs/client/connection.class.php
+++ srcs/client/connection.class.php
@@ -7,8 +7,9 @@ class		connection
 	private			$_sock;
 	private			$_port;
 	private			$_host;
+	static public	$verbose_debug = TRUE;
 	static public	$verbose_send = FALSE;
-	static public	$verbose_read = TRUE;
+	static public	$verbose_read = FALSE;
 
 	public	function	set_host($host)
 	{
@@ -24,20 +25,24 @@ class		connection
 	{
 		if (($this->_sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === FALSE)
 		{
-			echo socket_strerror(socket_last_error())."\n";
+			if (self::$verbose_debug === TRUE)
+				echo socket_strerror(socket_last_error()) . PHP_EOL;
 			exit (1);
 		}
 		else if (@socket_set_option($this->_sock, SOL_SOCKET, SO_REUSEADDR, 1) === FALSE)
 		{
-			echo "socket_set_option : " . socket_strerror(socket_last_error())."\n";
+			if (self::$verbose_debug === TRUE)
+				echo "socket_set_option : " . socket_strerror(socket_last_error()) . PHP_EOL;
 			exit (1);
 		}
 		else if (@socket_connect($this->_sock, $this->_host, $this->_port) === FALSE)
 		{
-			echo socket_strerror(socket_last_error())."\n";
+			if (self::$verbose_debug === TRUE)
+				echo socket_strerror(socket_last_error()) . PHP_EOL;
 			exit (1);
 		}
-		echo "Connecting client on server ".$this->_host.":".$this->_port." succeed.\n";
+		if (self::$verbose_debug === TRUE)
+			echo "Connecting client on server ".$this->_host.":".$this->_port." succeed." . PHP_EOL;
 	}
 
 	public	function	send_msg($msg)
@@ -45,7 +50,8 @@ class		connection
 		if (self::$verbose_send === TRUE) echo "--> $msg:";
 		if (@socket_write($this->_sock, trim($msg)."\n", strlen($msg) + 1) === FALSE)
 		{
-			echo "Impossible d'envoyer le message sur la socket " . $this->_sock."\n";
+			if (self::$verbose_debug === TRUE)
+				echo "Impossible d'envoyer le message sur la socket " . $this->_sock. PHP_EOL;
 			exit (1);
 		}
 	}
@@ -54,7 +60,8 @@ class		connection
 	{
 		if (($msg = @socket_read($this->_sock, 1024, PHP_NORMAL_READ)) === FALSE)
 		{
-			echo "Impossible de lire le message sur la socket " . $this->_sock."\n";
+			if (self::$verbose_debug === TRUE)
+				echo "Impossible de lire le message sur la socket " . $this->_sock . PHP_EOL;
 			exit (1);
 		}
 		return (trim($msg));
@@ -65,7 +72,7 @@ class		connection
 		$rep = $this->read_msg();
 		if ($rep === "elevation en cours")
 			$rep = $this->read_msg();
-		if (self::$verbose_read === TRUE) echo $rep . "\n";
+		if (self::$verbose_read === TRUE) echo $rep . PHP_EOL;
 		if ($rep === "mort")
 			$this->disconnect();
 		return ($rep);
@@ -74,7 +81,8 @@ class		connection
 	public	function	disconnect()
 	{
 		socket_close($this->_sock);
-		echo "Closing connection client on server : ".$this->_host.":".$this->_port." succeed.\n";
+		if (self::$verbose_debug === TRUE)
+			echo "Closing connection client on server : ".$this->_host.":".$this->_port." succeed." . PHP_EOL;
 		exit (0);
 	}
 }

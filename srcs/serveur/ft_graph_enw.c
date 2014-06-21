4ee416dfa33de19faa851409efd8210c67eb7b79
Jean-Daniel GRANET <jgranet@e1r8p9.42.fr>
2014-06-21 18:13:57 +0200

    modif serveur enw

--- srcs/serveur/ft_graph_enw.c
+++ srcs/serveur/ft_graph_enw.c
@@ -6,7 +6,7 @@
 /*   By: jgranet <jgranet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/06 17:39:04 by jgranet           #+#    #+#             */
-/*   Updated: 2014/06/19 16:59:28 by mlemort          ###   ########.fr       */
+/*   Updated: 2014/06/21 18:13:35 by jgranet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ static void	ft_put_enw(t_game *g, int i, int j, int num_cli)
 {
 	ft_putstr_fd("enw ", g->cls[i].cs);
 	ft_putnbr_fd(g->eggs[j].nb, g->cls[i].cs);
-	ft_putchar_fd(' ', g->cls[j].cs);
+	ft_putchar_fd(' ', g->cls[i].cs);
 	ft_putnbr_fd(num_cli, g->cls[i].cs);
-	ft_putchar_fd(' ', g->cls[j].cs);
+	ft_putchar_fd(' ', g->cls[i].cs);
 	ft_putnbr_fd(g->eggs[j].x, g->cls[i].cs);
-	ft_putchar_fd(' ', g->cls[j].cs);
+	ft_putchar_fd(' ', g->cls[i].cs);
 	ft_putnbr_fd(g->eggs[j].y, g->cls[i].cs);
-	ft_putchar_fd('\n', g->cls[j].cs);
+	ft_putchar_fd('\n', g->cls[i].cs);
 }
 
 void		ft_graph_enw(t_game *g, int num_cli)
